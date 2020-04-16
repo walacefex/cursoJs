f435182b9bba4b63b580d0d5ce24e2abb023576e
walacefex <walacefex01@gmail.com>
2020-04-16 14:03:34 -0300

    usando if

---
+++ aula-11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+    if(vel >= 60){
+        console.log(`Ǜocê ultrapassou a velocidade permitida. Multado!`)
+    }
+console.log(`Dirija sempre usando o cinto de segurança`);
---
+++ aula-11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "EUA";
+console.log(`Vivendo em ${pais}`);
+
+if(pais == "Brasil"){
+    console.log('Você é Brasileiro!');
+}else{
+    console.log('Você é Estrangeiro!');
+}
