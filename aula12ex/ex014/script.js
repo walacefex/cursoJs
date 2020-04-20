07554de2ac93dbc5fd12404ce199a5ca35fcc65c
walacefex <walacefex01@gmail.com>
2020-04-20 18:29:48 -0300

    Cirnado uma variavel para escrever na tela com frase do periodo do dia

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,7 @@
 function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('image');
+    var periodo = window.document.getElementById('periodo');
     var data = new Date();
     var hora = data.getHours();
 
@@ -10,13 +11,16 @@ function carregar() {
         //Bom dia
         img.src = 'img/foto-manha.jpg';
         document.body.style.backgroundColor = "#6f9aad";
+        periodo.innerHTML = `Bom Dia!`;
     } else if(hora >= 12 && hora <= 18){
         //Boa Tarde
         img.src = 'img/foto-tarde.jpg'
         document.body.style.backgroundColor = "#fdbf56";
+        periodo.innerHTML = `Boa Tarde!`;
     }else {
         //Boa Noite
         img.src = 'img/foto-noite.jpg'
         document.body.style.backgroundColor = "#001220";
+        periodo.innerHTML = `Boa Noite!`;
     }
 }
